e607ef73eef70e39a690109a90326f1a12406a4a
esmalazarte <esmalazarte@up.edu.ph>
2024-09-26 19:03:09 +0800

    initial spring boot setup

---
+++ src/main/java/com/example/tech_exam_api_java/TechExamApiJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.tech_exam_api_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechExamApiJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechExamApiJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tech_exam_api_java/TechExamApiJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tech_exam_api_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechExamApiJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
